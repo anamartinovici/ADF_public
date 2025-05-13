523ab45c95625caf19188f6228eb7b9aa6b778fd
anamartinovici <martinovici@rsm.nl>
2025-05-13 15:44:14 +0200

    final version of the examples

--- Cat_API/data_collection.R
+++ Cat_API/data_collection.R
@@ -1,19 +1,29 @@
 library("httr2")
 
+n_observations <- 500
+n_per_request <- 100
+n_requests <- n_observations / n_per_request
+
+list_responses <- vector(mode = "list", length = n_requests)
+
 base_url <- "https://api.thecatapi.com/v1/images/search"
 
 my_request <- httr2::request(base_url = base_url)
-my_request
 
-my_request <- my_request |>
-	httr2::req_url_query(api_key = Sys.getenv("CAT_API_KEY"),
-						 limit = 10)
+for (index_page in 1:n_requests) {
+	my_request <- my_request |>
+		httr2::req_url_query(api_key = Sys.getenv("CAT_API_KEY"),
+							 limit = 100,
+							 order = "ASC",
+							 page = index_page - 1)
+	
+	list_responses[[index_page]] <- my_request |>
+		httr2::req_perform()
+	
+	if (httr2::resp_status_desc(list_responses[[index_page]]) != "OK") {
+		stop("check this request")
+	}
+}
 
-my_request |>
-	httr2::req_dry_run()
-
-response <- my_request |>
-	httr2::req_perform()
-
-save(response,
+save(list_responses,
 	 file = here::here("Cat_API", "data_collection.RData"))
--- Cat_API/data_processing.R
+++ Cat_API/data_processing.R
@@ -3,9 +3,27 @@ library("tidyverse")
 
 load(here::here("Cat_API", "data_collection.RData"))
 
-response |>
-	httr2::resp_status_desc()
-
-content <- response |>
+obj1 <- list_responses[[1]]
+content1 <- obj1 %>%
 	httr2::resp_body_json()
 
+obj2 <- list_responses[[2]]
+content2 <- obj2 %>%
+	httr2::resp_body_json()
+
+list_content <- vector(mode = "list", length = length(list_responses))
+for (index_element in 1:length(list_responses)) {
+	list_content[[index_element]] <- list_responses[[index_element]] %>%
+		httr2::resp_body_json()
+}
+
+list_content <- flatten(list_content)
+
+length(list_content)
+names(list_content)
+
+
+url_list <- map(list_content, pluck, "url")
+url_vector <- unlist(url_list)
+
+length(unique(url_vector))
