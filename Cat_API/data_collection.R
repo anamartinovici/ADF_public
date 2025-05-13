b93c27a48f5d0acf2308bef8c7586e51fe88ae9b
anamartinovici <martinovici@rsm.nl>
2025-05-13 14:38:42 +0200

    getting 10 observations works

--- Cat_API/data_collection.R
+++ Cat_API/data_collection.R
@@ -3,8 +3,11 @@ library("httr2")
 base_url <- "https://api.thecatapi.com/v1/images/search"
 
 my_request <- httr2::request(base_url = base_url)
-
 my_request
+
+my_request <- my_request |>
+	httr2::req_url_query(api_key = Sys.getenv("CAT_API_KEY"),
+						 limit = 10)
 
 my_request |>
 	httr2::req_dry_run()
@@ -12,8 +15,6 @@ my_request |>
 response <- my_request |>
 	httr2::req_perform()
 
-response |>
-	httr2::resp_status()
 response |>
 	httr2::resp_status_desc()
 
